8a2c5121370a06f11954d32c2ff0add54f5a497c
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-01-30 16:48:06 +0100

    Update demo config

--- demo/jupyterhub_conf.py
+++ demo/jupyterhub_conf.py
@@ -13,16 +13,6 @@ c = get_config()  # noqa
 jupyterhub_moss.set_config(c)
 
 
-SINFO_OUTPUT = """unused 40 28+ 121/1191/0/1312 (null) 196000+ infinite
-partition_1 48 35+ 38/1642/0/1680 (null) 196000+ 1-00:00:00
-partition_2 7 128 116/780/0/896 gpu:GPU-MODEL:2 512000 12:00:00
-partition_3 28 40 114/1006/0/1120 (null) 310000 4:00:00
-other_mixed 10 64 152/488/0/640 gpu:OTHER-GPU:2(S:0-1) 455000+ 7-00:00:00
-other_mixed 94 28+ 153/3583/0/3736 (null) 196000+ 7-00:00:00
-"""
-c.MOSlurmSpawner.slurm_info_cmd = f'echo "{SINFO_OUTPUT}"'
-
-
 # Partition descriptions, see https://github.com/silx-kit/jupyterhub_moss#partition-settings
 c.MOSlurmSpawner.partitions = {
     "partition_1": {
@@ -85,12 +75,23 @@ c.MOSlurmSpawner.partitions = {
     },
 }
 
+# Uncomment the following to custom the options_form template
+# c.JupyterHub.template_paths = [os.path.join(os.path.dirname(__file__), "templates")]
 
-# JupyterHub
+# Mock SLURM sinfo command for the demo
+SINFO_OUTPUT = """unused 5/35/0/40 28+ 121/1191/0/1312 (null) 196000+ infinite
+partition_1 2/46/0/48 35+ 38/1642/0/1680 (null) 196000+ 1-00:00:00
+partition_2 2/5/0/7 128 116/780/0/896 gpu:GPU-MODEL:2 512000 12:00:00
+partition_3 4/24/0/28 40 114/1006/0/1120 (null) 310000 4:00:00
+other_mixed 3/7/0/10 64 152/488/0/640 gpu:OTHER-GPU:2(S:0-1) 455000+ 7-00:00:00
+other_mixed 6/86/0/94 28+ 153/3583/0/3736 (null) 196000+ 7-00:00:00
+"""
+c.MOSlurmSpawner.slurm_info_cmd = f'echo "{SINFO_OUTPUT}"'
+
+# Batchspawner: Do not run sudo for the demo
+c.BatchSpawnerBase.exec_prefix = ""
+
+# JupyterHub basic config
 c.JupyterHub.ip = "127.0.0.1"
 c.JupyterHub.hub_ip = "127.0.0.1"
 c.JupyterHub.port = 8000
-c.JupyterHub.template_paths = [os.path.join(os.path.dirname(__file__), "templates")]
-
-# Batchspawner
-c.BatchSpawnerBase.exec_prefix = ""  # Do not run sudo
