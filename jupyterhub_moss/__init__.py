421f6db1a0eca08f47a5be9bedfb0c1a60f6b448
Thomas VINCENT <thomas.vincent@esrf.fr>
2025-09-22 15:40:39 +0200

    Bump to v10.0.0

--- jupyterhub_moss/__init__.py
+++ jupyterhub_moss/__init__.py
@@ -3,7 +3,7 @@ from tornado.web import StaticFileHandler as _StaticFileHandler
 from .spawner import MOSlurmSpawner
 from .utils import local_path as _local_path
 
-version = "9.0.0"
+version = "10.0.0"
 
 STATIC_FORM_REGEX = r"/form/(.*)"
 STATIC_FORM_PATH = _local_path("form")
